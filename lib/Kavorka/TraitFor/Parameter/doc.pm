90ffdb93fbcca1209f7474a0fa0c99e0cce3c37d
Erik Carlsson <info@code301.com>
2015-01-11 14:40:37 +0100

    Init

---
+++ lib/Kavorka/TraitFor/Parameter/doc.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Kavorka::TraitFor::Parameter::doc;
+
+# VERSION
+
+
+
+1;
+
+# ABSTRACT: Short intro
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Kavorka::TraitFor::Parameter::doc;
+
+=head1 DESCRIPTION
+
+Kavorka::TraitFor::Parameter::doc is ...
+
+=head1 SEE ALSO
+
+=cut
